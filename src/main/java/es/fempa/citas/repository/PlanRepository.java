d690cb732d67dc2b1d71d4b4198eee762967b631
salva95 <eldoctorsilva@gmail.com>
2018-01-30 18:59:02 +0100

    Holi

--- src/main/java/es/fempa/citas/repository/PlanRepository.java
+++ src/main/java/es/fempa/citas/repository/PlanRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import es.fempa.citas.domain.Plan;
 
 public interface PlanRepository extends JpaRepository<Plan, Integer> {
-
+	public Plan findByIdPlan(Integer id);
 }
