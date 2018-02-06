b828f1537b40f9c8b2d18ae60f56177648332fa4
salva95 <eldoctorsilva@gmail.com>
2018-02-06 18:38:46 +0100

    Auto stash before merge of "master" and "origin/master"
    okay

--- src/main/java/es/fempa/citas/domain/Idioma.java
+++ src/main/java/es/fempa/citas/domain/Idioma.java
@@ -1,13 +1,9 @@
 package es.fempa.citas.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
--- src/main/java/es/fempa/citas/repository/UsuarioRepository.java
+++ src/main/java/es/fempa/citas/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package es.fempa.citas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import es.fempa.citas.domain.Usuario;
 
