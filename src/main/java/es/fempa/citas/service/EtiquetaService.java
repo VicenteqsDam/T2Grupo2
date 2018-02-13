99060dd8ca61dba924c3f2afb5ed21a9a7dd51fa
Pablo <pablo.diaz.92@outlook.com>
2018-02-13 19:48:18 +0100

    hhhh
    hhhhhh

--- src/main/java/es/fempa/citas/service/EtiquetaService.java
+++ src/main/java/es/fempa/citas/service/EtiquetaService.java
@@ -3,10 +3,12 @@ package es.fempa.citas.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import es.fempa.citas.domain.Etiqueta;
 import es.fempa.citas.repository.EtiquetaRepository;
 
+@Service
 public class EtiquetaService {
 
 	@Autowired
